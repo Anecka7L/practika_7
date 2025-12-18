dc97efbcd4c979c18732dc819aaafc9f0371324a
Anecka7L <165366413+Anecka7L@users.noreply.github.com>
2025-12-18 09:40:23 +0500

    Update login_user.php

--- ajax/login_user.php
+++ ajax/login_user.php
@@ -6,12 +6,12 @@
 	$password = $_POST['password'];
 	
 	// ищем пользователя
-	// $query_user = $mysqli->query("SELECT * FROM `users` WHERE `login`='".$login."' AND `password`= '".$password."';");
+	$query_user = $mysqli->query("SELECT * FROM `users` WHERE `login`='".$login."' AND `password`= '".$password."';");
 	
-	// $id = -1;
-	// while($user_read = $query_user->fetch_row()) {
-	// 	$id = $user_read[0];
-	// }
+	$id = -1;
+	while($user_read = $query_user->fetch_row()) {
+		$id = $user_read[0];
+	}
 	// $id = -1;
 	// $Query = $mysqli->prepare(query: "SELECT `id` FROM `users` WHERE `login` = ? AND `password` = ?;");
 	// $Query->bind_param(
@@ -25,18 +25,34 @@
 	// 	$_SESSION['user'] = $id;
 	// }
 	// echo md5(string: md5(string: $id));
-	$id = -1;
+	// $id = -1;
 
 // Правильное использование prepare и bind_param
-$query = $mysqli->prepare("SELECT `id` FROM `users` WHERE `login` = ? AND `password` = ?");
-$query->bind_param("ss", $login, $password);
-$query->execute();
-$query->bind_result($id);
-$query->fetch();
-$query->close();
+// $query = $mysqli->prepare("SELECT `id` FROM `users` WHERE `login` = ? AND `password` = ?");
+// $query->bind_param("ss", $login, $password);
+// $query->execute();
+// $query->bind_result($id);
+// $query->fetch();
+// $query->close();
 
 if($id != -1) {
     $_SESSION['user'] = $id;
+
+	$Ip = $_SERVER["REMOTE_ADDR"];
+	$DateStart = date(format: "Y-m-d H:i:s");
+
+	$Sql = "INSERT INTO `session`(`IdUser`, `Ip`, `DateStart`, `DateNow`) VALUES ({$id}, '{$Ip}', '{$DateStart}', '{$DateStart}')";
+	$mysqli->query(query: $Sql);
+
+	$Sql = "SELECT `Id` FROM `session` WHERE `DateStart` = '{$DateStart}';";
+	$Query = $mysqli->query(query: $Sql);
+	$Read = $Query->fetch_assoc();
+	$_SESSION["IdSession"] = $Read["Id"];
+
+	$Sql = "INSERT INTO ".
+	"`logs`(`Ip`, `IdUser`, `Date`, `TineOnline`, `Event`) ".
+	"VALUES ('{$Ip}','{$id}','{$DateStart}','00:00:00','Пользователь {$login} авторизовался.')";
+	$mysqli->query(query: $Sql);
 }
-echo md5(md5($id));
+//  echo md5(md5($id));
 ?>
