8acb485091bebee196495abd1a3120c6625ab0c6
Anecka7L <165366413+Anecka7L@users.noreply.github.com>
2025-12-21 21:59:11 +0500

    Add files via upload

--- logs.php
+++ logs.php
@@ -23,6 +23,15 @@
 		<title> Admin панель </title>
 		
 		<link rel="stylesheet" href="style.css">
+		<style>
+			table{
+				width: 100%;
+			}
+			td{
+				text-align: center;
+				padding: 10px;
+			}
+		</style>
 	</head>
 	<body>
 		<div class="top-menu">
@@ -38,11 +47,21 @@
 		<div class="space"> </div>
 		<div class="main">
 			<div class="content">
-				<input type="button" class="button" value="Выйти" onclick="logout()"/>
+				<!-- <input type="button" class="button" value="Выйти" onclick="logout()"/> -->
 				
 				<div class="name">Журнал событий</div>
 			
-			
+			<table border="1">
+				<tr>
+					<td style="width: 165px;">Дата и время</td>
+					<td style="width: 165px;">IP Пользователя</td>
+					<td style="width: 165px;">Время в сети</td>
+					<td style="width: 165px;">Статус</td>
+					<td>Произошедшее событие</td>
+				</tr>
+			</table>
+
+
 				<div class="footer">
 					© КГАПОУ "Авиатехникум", 2020
 					<a href=#>Конфиденциальность</a>
@@ -52,7 +71,42 @@
 		</div>
 		
 		<script>
-			
+			GetEvents();
+			function GetEvents(){
+				$.ajax({
+					url         : 'ajax/events/get.php',
+					type        : 'POST', // важно!
+					data        : null,
+					cache       : false,
+					dataType    : 'html',
+					processData : false,
+					contentType : false, 
+					// функция успешного ответа сервера
+					success: GetEventsAjax,
+					// функция ошибки
+					error: function( ){
+						console.log('Системная ошибка!');
+					}
+				});
+			}
+			function GetEventsAjax(_data){
+				console.log(_data);
+
+				let $Table = $("table > tbody");
+				let Events = JSON.parse(_data);
+
+				Events.forEach((Event) => {
+					$Table.append(`
+					<tr>
+						<td>${Event["Date"]}</td>
+						<td>${Event["Ip"]}</td>
+						<td>${Event["TimeOnline"]}</td>
+						<td>${Event["Status"]}</td>
+						<td style="text-align: left;">${Event["Event"]}</td>
+					</tr>
+					`);
+				})
+			}
 		</script>
 	</body>
 </html>
